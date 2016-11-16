a60a054dfff0ae3f6407a070c83b1081b00c82e6
warrenbrown <warren@katapultmedia.com>
2016-11-16 11:19:09 -0600

    added fuzy search by first last name email

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
+  get 'customers/index'
+
   devise_for :users
   get 'dashboard/index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-
+  resources :customer, only: [:index]
   # You can have the root of your site routed with "root"
   root 'dashboard#index'
 
