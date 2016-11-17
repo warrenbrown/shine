cdb5c9e113bb756846fd3bbad3acf06ea04ba1f8
warrenbrown <warren@katapultmedia.com>
2016-11-17 17:53:18 -0600

    Added angular

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
   resources :customer, only: [:index]
   # You can have the root of your site routed with "root"
   root 'dashboard#index'
+  get 'angular_test', to: 'angular_test#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
