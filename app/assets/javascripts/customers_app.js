05f1f8c9999030254880709e56ab3241b19925a9
warrenbrown <warren@katapultmedia.com>
2016-11-21 08:12:41 -0600

    added typehead with angular

---
+++ app/assets/javascripts/customers_app.js
@@ -0,0 +1,37 @@
+var app = angular.module('customers',[]);
+
+app.controller("CustomerSearchController", [
+          '$scope','$http',
+  function($scope , $http) {
+
+    var page = 0;
+
+    $scope.customers = [];
+    $scope.search = function(searchTerm) {
+      if (searchTerm < 3) {
+        return;
+      }
+
+      $http.get("/customers.json",
+                { "params": { "keywords": searchTerm, "page": page } }
+      ).success(
+        function(data,status,headers,config) {
+          $scope.customers = data;
+      }).error(
+        function(data,status,headers,config) {
+          alert("There was a problem: " + status);
+        });
+    }
+
+    $scope.previousPage = function() {
+      if (page > 0) {
+        page = page - 1;
+        $scope.search($scope.keywords);
+      }
+    }
+    $scope.nextPage = function() {
+      page = page + 1;
+      $scope.search($scope.keywords);
+    }
+  }
+]);
