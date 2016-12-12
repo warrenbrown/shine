2991cc973a6c87f5ccd82e037cb3930fc6ad9ab6
warrenbrown <warren@katapultmedia.com>
2016-12-12 11:59:26 -0600

    added ui to customers detail

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'dashboard#index'
-  resources :customers, only: [ :index ]
+  resources :customers, only: [ :index, :show ]
 
   get "angular_test", to: "angular_test#index"
 end
